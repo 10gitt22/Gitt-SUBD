941e2b40f6d534becdd9dd099250160945e95fa7
10gitt22 <46067435+10gitt22@users.noreply.github.com>
2020-12-18 09:47:18 +0200

    3 Lab

---
+++ SQLQueryLab3.sql
@@ -0,0 +1,11 @@
+﻿ALTER TABLE Товари
+ADD [Посилання] [nvarchar](255) NOT NULL;
+
+ALTER TABLE Товари
+DROP COLUMN Посилання
+
+ALTER TABLE Товари
+ALTER COLUMN [Ціна товару] int NOT NULL;
+
+ALTER TABLE Товари
+ADD CHECK ([Ціна товару]<=100000 AND [Ціна товару]>0)
