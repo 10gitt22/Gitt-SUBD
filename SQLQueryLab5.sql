2c725e1c4523aa5c379ab34a8bfef7af9a25264f
10gitt22 <46067435+10gitt22@users.noreply.github.com>
2021-01-13 13:52:16 +0200

    Add files via upload
    Lab 5

---
+++ SQLQueryLab5.sql
@@ -0,0 +1,12 @@
+﻿USE [MusicShopDb]
+
+SELECT * FROM Виробники
+UNION SELECT * FROM Виробники2
+
+SELECT * FROM Виробники
+ WHERE Виробники.[Код]  IN (SELECT [Код] FROM Виробники2);
+
+ SELECT * FROM Виробники
+ WHERE Виробники.[Код] NOT IN (SELECT [Код] FROM Виробники2);
+
+ SELECT * FROM Виробники, Виробники2;
